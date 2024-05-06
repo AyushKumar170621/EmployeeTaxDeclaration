d500a377fa976a1598afd6cb870622457df2b5fd
AyushKumar170621 <ayushk1701@gmail.com>
2024-05-07 03:04:18 +0530

    enhanced ui

--- EmployeeTaxDeclaration/obj/Debug/net8.0/EmployeeTaxDeclaration.AssemblyInfo.cs
+++ EmployeeTaxDeclaration/obj/Debug/net8.0/EmployeeTaxDeclaration.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeTaxDeclaration")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a0b9b7fd32b2240c5bb862c5c592bd803da26f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fedc59381a4f40e5d25f935b60f75eb627261bae")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeTaxDeclaration")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeTaxDeclaration")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
